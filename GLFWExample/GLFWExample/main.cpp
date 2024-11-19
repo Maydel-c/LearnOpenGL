84dae10d70ea3f663c02dee4f0547431a62b681a
Maydel-c <165515798+Maydel-c@users.noreply.github.com>
2024-11-19 20:08:30 +0530

    multiple colored vertices triangle

--- GLFWExample/GLFWExample/main.cpp
+++ GLFWExample/GLFWExample/main.cpp
@@ -6,22 +6,21 @@
 // vertex shader source code
 const char* vertexShaderSource = "#version 330 core\n"
 "layout (location=0) in vec3 aPos;\n"
-"layout (location=1) in vec4 ourColor;\n"
-"out vec4 vertexColor;\n"
+"layout (location=1) in vec3 aColor;\n"
+"out vec3 ourColor;\n"
 "void main()\n"
 "{\n"
     "gl_Position = vec4(aPos, 1);\n"
-    "vertexColor = ourColor;\n" // specify a color output to the fragment shader
+    "ourColor = aColor;\n" // specify a color output to the fragment shader
 "}\0";
 
 // fragment shader source code
 const char* fragmentShaderSource = "#version 330 core\n"
+"in vec3 ourColor;\n"
 "out vec4 FragColor;\n"
-//"in vec4 vertexColor;\n"
-"uniform vec4 ourColor;\n"
 "void main()\n"
 "{\n"
-    "FragColor = ourColor;\n"
+    "FragColor = vec4(ourColor, 1.0f);\n"
 "}\n\0";
 
 
@@ -142,6 +141,10 @@ int main()
     // Specify what the input data means by specifying the vertex attributes. input data can contain any form of data and we need to explicitly tell openGL what that data means
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6* sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
+    
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6* sizeof(float), (void*)(3*sizeof(float)));
+    glEnableVertexAttribArray(1);
+    
     glBindBuffer(GL_ARRAY_BUFFER, 0);
     glBindVertexArray(0);
     
